dafee2786695a76032a8abd0e09c0af1d4f62d0a
Hirusha Hewagamage <143105080+hiruim@users.noreply.github.com>
2024-10-04 22:55:43 +0530

    pay

--- lib/ui/screens/payment/paymant.dart
+++ lib/ui/screens/payment/paymant.dart
@@ -1,1 +1,16 @@
-Abc(),
+import 'package:flutter/material.dart';
+
+class Paymant extends StatelessWidget {
+  const Paymant({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Paymant'),
+      ),
+      body: Center(
+        child: Text('Paymant'),
+      ),
+    );
+  }
+}
