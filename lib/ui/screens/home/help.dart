8281ad084d4634abc1a6cb06b0ab60ea803bac8f
Hirusha Hewagamage <143105080+hiruim@users.noreply.github.com>
2024-10-09 09:10:16 +0530

    pages

--- lib/ui/screens/home/help.dart
+++ lib/ui/screens/home/help.dart
@@ -1,15 +1,96 @@
 import 'package:flutter/material.dart';
 
-class HelpScreen extends StatelessWidget {
-  const HelpScreen({Key? key}) : super(key: key);
+class HelpScreen extends StatefulWidget {
+  @override
+  _HelpState createState() => _HelpState();
+}
+
+class _HelpState extends State<HelpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Help'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Help",
+            style: TextStyle(
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(
+              color: Colors.black), // Ensures icon is visible on white AppBar
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 20),
+                _buildSectionWithTitleAndBorder('1. Route Information'),
+                _buildTextWithBorder(
+                  "Users can inquire about specific bus routes, stops, and schedules. "
+                  "Real-time tracking may be available to provide the current location of the school buses.",
+                ),
+                SizedBox(height: 20),
+                _buildSectionWithTitleAndBorder('2. Emergency Support'),
+                _buildTextWithBorder(
+                  "Immediate access to emergency contacts and protocols in case of incidents during bus transit. "
+                  "Quick assistance in situations such as breakdowns, accidents, or delays.",
+                ),
+                SizedBox(height: 20),
+                _buildSectionWithTitleAndBorder('3. Safety Guidelines'),
+                _buildTextWithBorder(
+                  "Offers information on safety protocols, rules, and regulations for students using the school bus. "
+                  "Tips for parents on ensuring their child's safety during transit.",
+                ),
+                SizedBox(height: 20),
+                _buildSectionWithTitleAndBorder('4. User-Friendly Interface'),
+                _buildTextWithBorder(
+                  "Simple and intuitive interface for easy navigation, making it accessible to users of all technical levels.",
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('Help'),
+    );
+  }
+
+  Widget _buildSectionWithTitleAndBorder(String title) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0), // Adds rounded corners
+      ),
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 9, 62, 116),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextWithBorder(String text) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0), // Adds rounded corners
+      ),
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+        ),
       ),
     );
   }
---
+++ lib/ui/screens/home/notification.dart
@@ -0,0 +1,143 @@
+import 'package:even_tick/config/app-color.dart';
+import 'package:even_tick/config/app_assets.dart';
+import 'package:even_tick/config/text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class NotificationPage extends StatelessWidget {
+  final List<Map<String, String>> notifications = [
+    {
+      'subject': 'Event 01',
+      'details':
+          'Pay from account 251897668546 to Test Event 1 Amount 349.00 LKR is successfully completed.',
+    },
+    {
+      'subject': 'Event 02',
+      'details':
+          'Pay from account 251897668547 to Test Event 2 Amount 3900.00 LKR is successfully completed.',
+    },
+    {
+      'subject': 'Event 03',
+      'details':
+          'Pay from account 251897668548 to Test Event 3 Amount 500.00 LKR is successfully completed.',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.buttonTextColor,
+        leading: IconButton(
+          icon: SvgPicture.asset(AppAssets.arrowBackWithCircularBoarderIcon),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        centerTitle: true,
+        title: const Text(
+          'Notification',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+            color: AppColors.headingColor,
+            height: 30,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: ListView.builder(
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              NotificationCard(
+                subject: notifications[index]['subject']!,
+                details: notifications[index]['details']!,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.5,
+                color: AppColors.iconButtonBoarderColor,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NotificationCard extends StatefulWidget {
+  final String subject;
+  final String details;
+
+  NotificationCard({
+    required this.subject,
+    required this.details,
+  });
+
+  @override
+  _NotificationCardState createState() => _NotificationCardState();
+}
+
+class _NotificationCardState extends State<NotificationCard> {
+  bool _isRead = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.subject,
+                style: notificationsubjectTextStyle,
+              ),
+              SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Text(
+                  widget.details,
+                  style: notificationdetailsTextStyle,
+                ),
+              ),
+            ],
+          ),
+          onTap: () {
+            setState(() {
+              _isRead = true;
+            });
+          },
+        ),
+        Positioned(
+          top: 2,
+          right: 20,
+          child: Container(
+            margin: EdgeInsets.only(right: 8),
+            decoration: BoxDecoration(
+              gradient: _isRead
+                  ? null
+                  : LinearGradient(
+                      colors: [
+                        AppColors.notificationcircleleftcolor,
+                        AppColors.notificationcirclerightcolor
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+              color: _isRead ? Colors.white : null,
+              shape: BoxShape.circle,
+            ),
+            width: 12,
+            height: 12,
+          ),
+        ),
+      ],
+    );
+  }
+}
