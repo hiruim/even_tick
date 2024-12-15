b3d8318584468512a9ce1618ee4e109d7e62df14
Tharindu127 <tharindu.athapaththu@arimaclanka.com>
2024-12-15 15:39:48 +0530

    firebase integration with authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:even_tick/ui/screens/login/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
